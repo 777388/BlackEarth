d02446b2c81819dfb471467037a6d6bd7214a044
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-12-26 05:52:42 -0800

    Update degrees.py

--- degrees.py
+++ degrees.py
@@ -8,9 +8,9 @@ for i in range(int(sys.argv[1])):
     b.append(i)
 
 if sys.argv[2] == "deg":
-    W = lambda x:  print(str(t.forward(x % 10 + int(sys.argv[3]))) + "\n" + sys.argv[5] + "\n" + str(t.left(math.degrees(x) % int(sys.argv[4]) + int(sys.argv[3]))) + "\n" + sys.argv[6] + " \n" + str(t.pos())) #+ "number " + str(x) + "\r\n" + str(math.degrees(x)) + " Degrees" + "\r\n"  + str(math.radians(x)) + " Radians" + "\r\n" + str(math.sinh(x)) + " Hyperbolic Sines" + "\r\n" + str(math.asinh(x)) + " Inverse Hyperbolic Sines" + "\r\n")
+    W = lambda x:  print(str(t.forward(x % 10 + int(sys.argv[3]))) + "\n" + str(sys.argv[5]) + "\n" + str(t.left(math.degrees(x) % int(sys.argv[4]) + int(sys.argv[3]))) + "\n" + str(sys.argv[6]) + " \n" + str(t.pos())) #+ "number " + str(x) + "\r\n" + str(math.degrees(x)) + " Degrees" + "\r\n"  + str(math.radians(x)) + " Radians" + "\r\n" + str(math.sinh(x)) + " Hyperbolic Sines" + "\r\n" + str(math.asinh(x)) + " Inverse Hyperbolic Sines" + "\r\n")
 if sys.argv[2] == "rad":
-    W = lambda x:  print(str(t.forward(x % 10 + int(sys.argv[3]))) +  "\n" + sys.argv[6] + "\n"+ str(t.right(math.radians(x) % int(sys.argv[4]) + int(sys.argv[3]))) + "\n" + sys.argv[5] + " \n" + str(t.pos()))
+    W = lambda x:  print(str(t.forward(x % 10 + int(sys.argv[3]))) +  "\n" + str(sys.argv[6]) + "\n"+ str(t.right(math.radians(x) % int(sys.argv[4]) + int(sys.argv[3]))) + "\n" + str(sys.argv[5]) + " \n" + str(t.pos()))
 print(list(map(W, b)))
 turtle.mainloop()
 
